e792f9af3f2fdec2394788e1e7f93248d33d1d40
Tonny Bukdahl <tonnybukdahl@users.noreply.github.com>
2019-10-29 19:36:11 +0100

    Create lists.py - example code

---
+++ lists.py
@@ -0,0 +1,33 @@
+shopList = ["apples", "oranges", "bananas", "cheese"]
+
+shopList
+
+['apples', 'oranges', 'bananas', 'cheese']
+shopList[0]
+
+shopList[2]
+
+shopList[0:2]
+
+shopList[0:3]
+
+shopList.append("blues")
+shopList
+
+shopList[0] = "cherries"
+shopList
+
+del shopList[1]
+shopList
+
+len(shopList)
+
+shopList2 = ["bread", "jam", "butter"]
+shopList + shopList2
+
+shopList * 2
+
+listNum = [1,2,3,4,5]
+max(listNum)
+
+min(listNum)
